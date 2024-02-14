baa9bf52fd8713f6bb5c17259949f3be3db25586
Yasmine <yasmine.pvt@gmail.com>
2024-02-14 15:32:36 +0100

    favicon

--- src/app/(home)/layout.js
+++ src/app/(home)/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <head>
-        <link rel='icon' href='/favicon.ico'/>
+        <link rel='icon' href='/icon.png'/>
       </head>
       <body className={inter.className}>
         <Sidemenu/>
