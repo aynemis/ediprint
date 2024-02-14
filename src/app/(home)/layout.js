bdc1017e416279ce53693bac8a74a3dca49112b6
Yasmine <yasmine.pvt@gmail.com>
2024-02-14 15:18:41 +0100

    favicon

--- src/app/(home)/layout.js
+++ src/app/(home)/layout.js
@@ -12,10 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <link rel='icon' href='/favicon.ico' />
-      </head>
+    <html lang="fr">
       <body className={inter.className}>
         <Sidemenu/>
         {children}
