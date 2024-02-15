146e1cf9c86ac113c2d2d0292aa5ed965cc73d38
Yasmine <yasmine.pvt@gmail.com>
2024-02-16 00:42:43 +0100

    products description

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -7,7 +7,7 @@ export default function Home() {
 
 
   return (
-      <main >
+      <main className='flex flex-col items-center'>
         <ProductsIntro />
         <ProductDesc />
       </main>
