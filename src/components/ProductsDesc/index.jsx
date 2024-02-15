02f16ff9eab8d8a8c1b844514558ebe034926a70
Yasmine <yasmine.pvt@gmail.com>
2024-02-16 00:08:34 +0100

    products description

--- src/components/ProductsDesc/index.jsx
+++ src/components/ProductsDesc/index.jsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export default function Index() {
     return (
-        <div className='py-6 sm:py-8 lg:py-12 lg:px-12'>
-    <div  className="bg-gray-800 mb-12 rounded-lg px-12 py-6 lg:py-12">
+    <div className='py-6 sm:py-8 lg:py-12 lg:px-12'>
+    <div  className="bg-gray-800 mb-12 rounded-lg px-12 py-6 lg:py-12 text-white">
         <div id="papeterie">
             <h2 className='text-xl font-bold lg:text-2xl mb-4'>Papeterie</h2>
             <li className='text-lg'>Agenda</li>
@@ -12,21 +12,21 @@ export default function Index() {
             <li className='text-lg'>Bloc notes</li>
         </div>
     </div>
-    <div  className="bg-gray-800 mb-12 rounded-lg px-12 py-6 lg:py-12">   
+    <div  className="bg-gray-800 mb-12 rounded-lg px-12 py-6 lg:py-12 text-white">   
         <div id="pub">
             <h2 className='text-xl font-bold lg:text-2xl mb-4'>Publicité PLV</h2>
             <li className='text-lg'>Présentoirs</li>
             <li className='text-lg'>Kakemonos</li>
         </div>
     </div> 
-    <div  className="bg-gray-800 mb-12 rounded-lg px-12 py-6 lg:py-12"> 
+    <div  className="bg-gray-800 mb-12 rounded-lg px-12 py-6 lg:py-12 text-white"> 
         <div id="packaging">
             <h2 className='text-xl font-bold lg:text-2xl mb-4'>Packaging</h2>
             <li className='text-lg'>Sacs</li>
             <li className='text-lg'>Boites</li>
         </div>
     </div>
-    <div  className="bg-gray-800 mb-12 rounded-lg px-12 py-6 lg:py-12">    
+    <div  className="bg-gray-800 mb-12 rounded-lg px-12 py-6 lg:py-12 text-white">    
         <div id="goodies">
             <h2 className='text-xl font-bold lg:text-2xl mb-4'>Goodies</h2>
             <li className='text-lg'>Gourdes</li>
--- src/components/ProductsIntro/index.jsx
+++ src/components/ProductsIntro/index.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Index() {
     return (
-    <div className="h-screen h-full py-6 sm:py-8 lg:py-12 lg:px-12">    
+    <div className="py-6 sm:py-8 lg:py-12 lg:px-12">    
         <div className="mx-auto max-w-screen-2xl py-6  lg:py-12 px-4 md:px-8">
             <div className="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12">
                 <div className="flex items-center gap-12">
