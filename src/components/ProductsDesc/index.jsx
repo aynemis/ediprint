c1e129f4560dfe2b412b5536209a26726650067a
Yasmine <yasmine.pvt@gmail.com>
2024-02-16 00:06:05 +0100

    products description

--- src/components/ProductsDesc/index.jsx
+++ src/components/ProductsDesc/index.jsx
@@ -4,33 +4,33 @@ import Link from 'next/link';
 export default function Index() {
     return (
         <div className='py-6 sm:py-8 lg:py-12 lg:px-12'>
-    <div  className="bg-gray-800 py-6 lg:py-12">
+    <div  className="bg-gray-800 mb-12 rounded-lg px-12 py-6 lg:py-12">
         <div id="papeterie">
-            <h2 className='text-xl font-bold lg:text-2xl text-white'>Papeterie</h2>
-            <li>Agenda</li>
-            <li>Carnets</li>
-            <li>Bloc notes</li>
+            <h2 className='text-xl font-bold lg:text-2xl mb-4'>Papeterie</h2>
+            <li className='text-lg'>Agenda</li>
+            <li className='text-lg'>Carnets</li>
+            <li className='text-lg'>Bloc notes</li>
         </div>
     </div>
-    <div  className="bg-gray-800 py-6 lg:py-12">   
+    <div  className="bg-gray-800 mb-12 rounded-lg px-12 py-6 lg:py-12">   
         <div id="pub">
-            <h2 className='text-xl font-bold lg:text-2xl text-white'>Publicité PLV</h2>
-            <li>Présentoirs</li>
-            <li>Kakemonos</li>
+            <h2 className='text-xl font-bold lg:text-2xl mb-4'>Publicité PLV</h2>
+            <li className='text-lg'>Présentoirs</li>
+            <li className='text-lg'>Kakemonos</li>
         </div>
     </div> 
-    <div  className="bg-gray-800 py-6 lg:py-12"> 
+    <div  className="bg-gray-800 mb-12 rounded-lg px-12 py-6 lg:py-12"> 
         <div id="packaging">
-            <h2 className='text-xl font-bold lg:text-2xl text-white'>Packaging</h2>
-            <li>Sacs</li>
-            <li>Boites</li>
+            <h2 className='text-xl font-bold lg:text-2xl mb-4'>Packaging</h2>
+            <li className='text-lg'>Sacs</li>
+            <li className='text-lg'>Boites</li>
         </div>
     </div>
-    <div  className="bg-gray-800 py-6 lg:py-12">    
+    <div  className="bg-gray-800 mb-12 rounded-lg px-12 py-6 lg:py-12">    
         <div id="goodies">
-            <h2 className='text-xl font-bold lg:text-2xl text-white'>Goodies</h2>
-            <li>Gourdes</li>
-            <li>Parapluies</li>
+            <h2 className='text-xl font-bold lg:text-2xl mb-4'>Goodies</h2>
+            <li className='text-lg'>Gourdes</li>
+            <li className='text-lg'>Parapluies</li>
         </div>
     </div>
     </div>
